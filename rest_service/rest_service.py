6067b9108400e9be2ac65849a858e969c461f8b5
Oleksandr <skipfromua@gmail.com>
2020-08-12 14:12:30 +0300

    little fix

--- rest_service/rest_service.py
+++ rest_service/rest_service.py
@@ -6,14 +6,14 @@ from .dropbox_api import download, upload, dbx_object
 app = Flask(__name__)
 
 
-@app.route('/binary_file/<int:key_value>', methods=['GET'])
+@app.route('/binaryFile/<int:key_value>', methods=['GET'])
 def hello_world(key_value):
     data = download(dbx_object, '{}.json'.format(key_value))
     print(data)
     return jsonify({key_value: data.decode('utf-8')})
 
 
-@app.route('/binary_file', methods=['POST'])
+@app.route('/createBinaryFile', methods=['POST'])
 def post_request():
     if not request.json:
         abort(400)
