487487b1eb3090a33e18dc894dfd84cefe475067
Oleksandr <skipfromua@gmail.com>
2020-08-13 14:42:41 +0300

    refactor

--- rest_service/dropbox_api.py
+++ rest_service/dropbox_api.py
@@ -10,7 +10,6 @@ def upload(dbx_object, name, data_to_save, overwrite=True):
             if overwrite
             else dropbox.files.WriteMode.add)
     data = data_to_save
-    print(data)
     res = dbx_object.files_upload(
         data, path, mode,
         mute=True)
@@ -19,6 +18,8 @@ def upload(dbx_object, name, data_to_save, overwrite=True):
 
 def download(dbx_object, name):
     path = '/binary_data_storage/' + name
-    md, res = dbx_object.files_download(path)
+    res = dbx_object.files_download(path)
     data = res.content
     return data
+
+
--- rest_service/handlers.py
+++ rest_service/handlers.py
@@ -9,19 +9,31 @@ def download_data_handler(key_value):
 
 def upload_data_handler(request):
     if request.headers['Content-Type'] == 'text/plain':
-        hashed_data = abs(hash(request.data))
-        upload(dbx_object, '{}'.format(hashed_data), request.data)
-        return "200 Key-value:{}".format(hashed_data)
+        return "200 Key-value:{}".format(_text_plain_parse(request))
 
     elif request.headers['Content-Type'] == 'application/json':
-        if not request.json:
-            abort(400)
-        upload(dbx_object, '{}'.format(request.json['key_value']), bytes(request.json['data'].encode()))
-        return "200 JSON Message: " + json.dumps(request.json)
+        return "200 JSON Message: {}".format(_application_json_parse(request))
 
     elif request.headers['Content-Type'] == 'application/octet-stream':
-        hashed_data = abs(hash(request.data))
-        upload(dbx_object, '{}'.format(hashed_data), request.data)
-        return "200 Key-value:{}".format(hashed_data)
+        return "200 Key-value:{}".format(_application_octet_stream_parse(request))
     else:
         return "415 Unsupported Media Type ;)"
+
+
+def _text_plain_parse(request):
+    hashed_data = abs(hash(request.data))
+    upload(dbx_object, '{}'.format(hashed_data), request.data)
+    return hashed_data
+
+
+def _application_json_parse(request):
+    if not request.json:
+        abort(400)
+    upload(dbx_object, '{}'.format(request.json['key_value']), bytes(request.json['data'].encode()))
+    return json.dumps(request.json)
+
+
+def _application_octet_stream_parse(request):
+    hashed_data = abs(hash(request.data))
+    upload(dbx_object, '{}'.format(hashed_data), request.data)
+    return hashed_data
